95aa1b1e011933a3c2f71e2c51f29d3ee05346ac
Nicolas GARIN <nicolas.garin@soat.fr>
2016-11-24 17:05:38 +0100

    [ADD] Command line to create a component

---
+++ bin/component/component.file.js
@@ -0,0 +1,23 @@
+module.exports = `import { Component, Inject } from '@angular/core'
+
+import { AppActions } from '{{appPath}}'
+
+@Component({
+    selector: '{{selector}}',
+    template: '',
+})
+
+export class {{componentClassName}} {
+    private appStore
+
+    constructor (
+        @Inject('AppStore') appStore
+    ) {
+        this.appStore = appStore
+    }
+
+    public ngOnInit() {
+        this.appStore.dispatch(AppActions.setAppState({ loading: true }))
+    }
+}
+`
