a6f1308ee93e1bb841951f3d3ec21b9167f10d36
Nicolas GARIN <nicolas.garin@soat.fr>
2017-06-29 14:26:29 +0200

    [BUG] Build for unit tests (webpack)

--- build/test.js
+++ build/test.js
@@ -2,7 +2,7 @@ var webpackConfig = require('./webpack.base')
 
 module.exports = () => {
     webpackConfig.devtool = 'inline-source-map'
-    webpackConfig.module.preLoaders = []
+    webpackConfig.module.preLoaders.shift()
 
     return webpackConfig
 }
